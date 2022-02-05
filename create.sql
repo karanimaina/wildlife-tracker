0189f94a130d8015a1ca583c26982ba54d2ba7bb
felixmaina <karani.maina2010@gmail.com>
2022-02-05 19:29:44 +0300

    edit the create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE wildlife_tracker;
 \c wildlife_tracker
 CREATE TABLE animals(id  serial PRIMARY KEY,name varchar, age varchar,health varchar );
+ALTER TABLE  animals ADD COLUMN type varchar;
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -1,3 +1,5 @@
+import org.sql2o.Connection;
+
 public class EndangeredAnimals extends  Animals {
     private String health;
     private String age;
@@ -11,6 +13,29 @@ public class EndangeredAnimals extends  Animals {
 
     public EndangeredAnimals(String name, String type , String health, String age) {
         super(name, age, health);
+        this.type=type;
+        this.health=health;
+        this.age=age;
+    }
+
+    @Override
+    public void save() {
+        if(this.name.equals("")||this.type.equals("")||this.health.equals("")||this.age.equals("")){
+            throw new IllegalArgumentException("Fields cannot be left blank");
+        }
+        try(Connection conn = DB.sql2o.open()){
+            String sql ="INSERT INTO animals (name,type,health,age) VALUES (:name,:type,:health,:age)";
+            this.id=(int) conn.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .addParameter("health",this.health)
+                    .addParameter("age",this.age)
+                    .executeUpdate()
+                    .getKey();
+        }
+        }
+
 
     }
-}
+
+
