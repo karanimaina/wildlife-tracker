ad589bc2eb57ff3c1edc86975c6a206f7595f05b
felixmaina <karani.maina2010@gmail.com>
2022-02-05 11:43:39 +0300

    add type in animal class

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,5 +1,23 @@
-public class Animals {
+import java.util.List;
 
+public class Animals {
+public String name;
+public String  type;
+public static final String CATEGORY= "not endangered";
     public Animals(String name) {
+        this.name = name;
+        this.type = CATEGORY;
+    }
+
+
+
+    public String getName() {
+        return  name;
+    }
+
+
+
+    public String getType() {
+        return type;
     }
 }
--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -3,18 +3,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimalsTest {
-    private Animals setupAnimals(){
+    private Animals setupAnimals() {
         return new Animals("Zebra");
     }
 
     @Test
-    public void Animal_instantiatesCorrectly(){
+    public void Animal_instantiatesCorrectly() {
         Animals animals = setupAnimals();
         assertTrue(animals instanceof Animals);
     }
 
     @Test
     public void Animal_getsNameOfSavedAnimal() {
+        Animals animals = setupAnimals();
+        assertEquals("Zebra", animals.getName());
+    }
+
+    @Test
+    public void Animal_returnsCategoryOfSavedAnimal() {
+        Animals animals = setupAnimals();
+        assertEquals(" not endangered",animals.getType());
+    }
+
+    @Test
+    public void Animal_allInstancesAreSaved() {
+        Animals animals = setupAnimals();
+        animals.save();
+        assertTrue(Animals.all().get(0).equals(animals));
 
     }
 }
