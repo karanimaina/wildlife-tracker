5eda5e6ddcd1ebcd925f5fb8a46ff2bc4aed19a1
felixmaina <karani.maina2010@gmail.com>
2022-02-05 18:47:39 +0300

    add a construector

--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -1,2 +1,6 @@
-public class EndangeredAnimals {
+public class EndangeredAnimals extends  Animals {
+
+    public EndangeredAnimals(String name, String type , String health, String age) {
+        super(name, age, health);
+    }
 }
---
+++ src/test/java/EndangeredAnimalsTest.java
@@ -0,0 +1,20 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EndangeredAnimalsTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    @Test
+    public void testInstanceOfEndangeredAnimalsClass_true(){
+        EndangeredAnimals testAnimal= setUpNewAnimal();
+        assertEquals(true,testAnimal instanceof EndangeredAnimals);
+    }
+    private EndangeredAnimals setUpNewAnimal() {
+        return new EndangeredAnimals("White RHino","endangered","healthy","young");
+    }
+
+
+}
