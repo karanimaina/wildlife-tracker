9856a4a85b801cc76c1f5ec77ddc0d0fdf883603
felixmaina <karani.maina2010@gmail.com>
2022-02-05 12:09:12 +0300

    extend the Database rule classfrom external resource  and generate Query that delete animals after every test that runs

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,18 @@
-public class DatabaseRule {
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+    @Override
+    protected  void  before(){
+        DB.sql2o= new Sql2o ("jdbc:postgresql://localhost:5432/wildlife-tracker", "karani-dev", "felixmaina");
+    }
+    @Override
+    protected void after() {
+        try(Connection conn =DB.sql2o.open()){
+            String deleteAnimals ="DELETE FROM animals";
+            conn.createQuery(deleteAnimals).executeUpdate();
+        }
+    }
+
 }
