ed35a3557275fbe1d68d9cc516643ae587ec9ba5
felixmaina <karani.maina2010@gmail.com>
2022-02-05 12:55:16 +0300

    add drop.sql

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ import org.sql2o.Sql2o;
 public class DatabaseRule extends ExternalResource {
     @Override
     protected  void  before(){
-        DB.sql2o= new Sql2o ("jdbc:postgresql://localhost:5432/wildlife-tracker", "karani-dev", "felixmaina");
+        DB.sql2o= new Sql2o ("jdbc:postgresql://localhost:5432/wildlife_tracker", "karani-dev", "felixmaina");
     }
     @Override
     protected void after() {
