ae802a14a9de6ed8f5d2b7518bf6851a57a1d2b8
felixmaina <karani.maina2010@gmail.com>
2022-02-05 19:51:56 +0300

    add a getter for name

--- src/test/java/Rangers.java
+++ src/test/java/Rangers.java
@@ -1,4 +1,18 @@
 public class Rangers {
+    private String name;
+    private String badge_number
+
+    public Rangers(String name, String badge_number, String phone_number) {
+        this.name = name;
+        this.badge_number=  badge_number;
+    }
 
 
+    public String getNane() {
+        return name;
+    }
+
+   // public String getBadgeNumber() {
+   //     return badge_number;
+   // }
 }
--- src/test/java/RangersTest.java
+++ src/test/java/RangersTest.java
@@ -1,5 +1,32 @@
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RangersTest {
+    @Rule
+    public DatabaseRule databaseRule=new DatabaseRule();
+
+    @Test
+    public void createInstanceOfRangersClass_true(){
+        Rangers ranger= setupNewRanger();
+        assertEquals(true,ranger instanceof Rangers);
+    }
+
+    @Test
+    public void Ranger_getsName() {
+        Rangers rangers = setupNewRanger();
+        assertEquals("Felix maina",rangers.getNane());
+    }
+    @Test
+    public void Ranger_getsBadgeNumber() {
+        Rangers rangers = setupNewRanger();
+        assertEquals("143234",rangers.getBadgeNumber());
+    }
+
+    private Rangers setupNewRanger(){
+        return new Rangers("Felix maina","143234","071234567");
+    }
+
 
 }
