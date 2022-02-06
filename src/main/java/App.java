553fd74f5b5940927f66b40cb08f4069d42d33ea
felixmaina <karani.maina2010@gmail.com>
2022-02-06 19:46:56 +0300

    add ranger form .hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,9 +12,19 @@ public class App {
             Map<String,Object> model=new HashMap<String, Object>();
             return new ModelAndView(model,"welcome.hbs");
         },new HandlebarsTemplateEngine());
-//getting the ranger form
+     //getting the ranger form
         get("/create/ranger",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"ranger-form.hbs");
+        },new HandlebarsTemplateEngine());
+        //create a new ranger object
+        post("/create/ranger/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            String name=request.queryParams("name");
+            String badge_number=request.queryParams("badge");
+            String phone_number=request.queryParams("phone");
+            Rangers ranger=new Rangers(name,badge_number,phone_number);
+            ranger.save();
             return new ModelAndView(model,"ranger-form.hbs");
         },new HandlebarsTemplateEngine());
 
