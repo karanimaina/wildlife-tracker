a42faa5990e1635a554dcff9dacc2b0ce28c3d68
felixmaina <karani.maina2010@gmail.com>
2022-02-06 12:25:50 +0300

    add table locationsin  create.sql

--- create.sql
+++ create.sql
@@ -3,4 +3,5 @@ CREATE DATABASE wildlife_tracker;
 CREATE TABLE animals(id  serial PRIMARY KEY,name varchar, age varchar,health varchar );
 ALTER TABLE  animals ADD COLUMN type varchar;
 CREATE TABLE rangers (id serial PRIMARY KEY,badge_number varchar,phone_number varchar);
+CREATE TABLE locations (id serial PRIMARY KEY, name varchar);
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
