cb1ee7a754d9f9feaacbd9c683f0dae5befea5ff
felixmaina <karani.maina2010@gmail.com>
2022-02-06 19:30:26 +0300

    add layout.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,16 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        //welcome page
         get("/",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
             return new ModelAndView(model,"welcome.hbs");
         },new HandlebarsTemplateEngine());
-
+//getting the ranger form
+        get("/create/ranger",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"ranger-form.hbs");
+        },new HandlebarsTemplateEngine());
 
 
 
