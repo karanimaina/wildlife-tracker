3f422f3c18b823ababe2068bf8a49502422dc34d
felixmaina <karani.maina2010@gmail.com>
2022-02-06 16:29:03 +0300

    add a getter for the animal_id and ranger_id

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,5 +1,28 @@
 public class Sightings {
+private  int id;
+private int location_id;
+private int ranger_id;
+private  int  animal_id
 
-    public Sightings(int i, int i1, int i2) {
+    public Sightings(int location_id, int ranger_id, int  animal_id) {
+   this.location_id = location_id;
+   this.ranger_id = ranger_id;
+   this.animal_id = animal_id;
+    }
+    public int getId() {
+        return id;
+    }
+
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public int getRanger_Id() {
+        return ranger_id;
+    }
+
+    public int getAnimal_Id() {
+        return  animal_id;
     }
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -10,11 +10,38 @@ public class SightingsTest {
 
     @Test
     public void createInstanceOfSightingsClass_true() {
-
         Sightings sighting= setupSightings();
         assertEquals(true,sighting instanceof Sightings);
+
     }
+    @Test
+    public void allInstancesAreSaved() {
+        Sightings sightings = setupSightings();
+        Sightings otherSighting = new Sightings(1, 1, 1);
+        sightings.save();
+        otherSighting.save();
+        assertTrue(Sightings.find(sightings.getId()).equals(sightings));
+    }
+
+    @Test
+    public void returnsLocationId() {
+       Sightings sightings = setupSightings();
+       assertEquals(3,sightings.getLocation_id());
+    }
+    @Test
+    public void returnsLRangerId() {
+        Sightings sightings = setupSightings();
+        assertEquals(2,sightings.getRanger_Id());
+    }
+    @Test
+    public void returnsAmimal_Id() {
+        Sightings sightings = setupSightings();
+        assertEquals(7,sightings.getAnimal_Id());
+    }
+
+
     private  Sightings setupSightings(){
         return  new Sightings(2,3,7);
     }
+
 }
