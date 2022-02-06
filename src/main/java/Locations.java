5519bd075c83e6a25bc4ea30baa463c2c303b6f9
felixmaina <karani.maina2010@gmail.com>
2022-02-06 11:36:09 +0300

    add a constructor in locations class

--- src/main/java/Locations.java
+++ src/main/java/Locations.java
@@ -1,3 +1,5 @@
 public class Locations {
 
+    public Locations(String name) {
+    }
 }
--- src/test/java/LocationsTest.java
+++ src/test/java/LocationsTest.java
@@ -1,6 +1,15 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LocationsTest {
-
+    @Test
+    public void createInstanceOfLocationsClass() {
+        Locations location=setupLocations();
+        assertEquals(true,location instanceof Locations);
+    }
+private Locations  setupLocations(){
+    return new Locations("Near the river");
+}
 
 }
