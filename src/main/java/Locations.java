1fd344aae41eb5e140ea239a1181889db63b8ea2
felixmaina <karani.maina2010@gmail.com>
2022-02-06 11:49:41 +0300

    add a getter for the name

--- src/main/java/Locations.java
+++ src/main/java/Locations.java
@@ -1,5 +1,10 @@
 public class Locations {
-
+private String name;
     public Locations(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return  name;
     }
 }
--- src/test/java/LocationsTest.java
+++ src/test/java/LocationsTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.sql2o.Sql2oException;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +9,26 @@ public class LocationsTest {
         Locations location=setupLocations();
         assertEquals(true,location instanceof Locations);
     }
-private Locations  setupLocations(){
+
+    @Test
+    public void Locations_getsNameSuccesfuly() {
+        Locations locations = setupLocations();
+        assertEquals("Near the river",locations.getId());
+    }
+
+
+    @Test
+    public void saveLocationInstances() {
+        Locations locations = setupLocations();
+        try{
+            locations.save();
+            assertTrue(Locations.all().get(0).equals(locations));
+        }catch (IllegalArgumentException  ex){
+        System.out.println(ex);
+    }
+    }
+
+    private Locations  setupLocations(){
     return new Locations("Near the river");
 }
 
